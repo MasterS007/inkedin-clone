cd24b9afbad6b6c1f132e17a261e295ff2f5fd52
MasterS007 <munirasirajum13@gmail.com>
2022-06-13 16:40:13 +0600

    side bar almost done

--- linkiedin-clone/pages/index.js
+++ linkiedin-clone/pages/index.js
@@ -1,22 +1,31 @@
 import Head from "next/head";
 import HeaderDashboard from "../components/HeaderDashboard";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="bg-gray-900">
+    <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
       <Head>
-        <title>LinkiedIn App</title>
+        <title>feed | LinkiedIn</title>
         <meta name="locale" content="en_US" />
         <link
           rel="icon"
           href="https://static.licdn.com/scds/common/u/images/logos/favicons/v1/favicon.ico"
         />
       </Head>
-      {/* <div>
-        <button onClick={signOut}>Sign out</button>
-      </div> */}
 
       <HeaderDashboard />
+
+      <main className="flex justify-center gap-x-5 px-4 sm:px-12">
+        <div className="flex flex-col md:flex-row gap-5">
+          {/* sidebar */}
+          <Sidebar />
+
+          {/* feed */}
+        </div>
+
+        {/* widget */}
+      </main>
     </div>
   );
 }
