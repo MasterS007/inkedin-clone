d4b07c709485491c5bf37118eea248665a9f8467
MasterS007 <munirasirajum13@gmail.com>
2022-06-15 01:13:05 +0600

    recoil instaled

--- linkiedin-clone/components/Input.js
+++ linkiedin-clone/components/Input.js
@@ -1,14 +1,21 @@
+import {
+  ArticleOutlined,
+  EventSharp,
+  PhotoSizeSelectActual,
+  VideoCameraBack,
+} from "@mui/icons-material";
 import { Avatar } from "@mui/material";
 import { motion } from "framer-motion";
 import { useSession } from "next-auth/react";
 
 function Input() {
   const { data: session } = useSession();
+  console.log(session);
   return (
     <div className="bg-white dark:bg-[#1D2226] rounded-lg p-3 space-y-3 border border-x-gray-300 dark:border-none ">
       <div className="flex items-center space-x-2">
         <Avatar
-          src={session ? session.user.image : ""}
+          src={session?.user ? session.user.image : ""}
           className="!h-10 !w-10 !cursor-pointer"
         />
         {/* modal */}
@@ -20,6 +27,27 @@ function Input() {
         >
           Start a post
         </motion.button>
+      </div>
+
+      {/* icons */}
+
+      <div className="flex items-center flex-wrap gap-4 justify-center md:gap-10">
+        <button className="inputButton group">
+          <PhotoSizeSelectActual className="text-blue-400" />
+          <h4 className="opacity-80 group-hover:opacity-100">Photo</h4>
+        </button>
+        <button className="inputButton group">
+          <VideoCameraBack className="text-green-400" />
+          <h4 className="opacity-80 group-hover:opacity-100">Video</h4>
+        </button>
+        <button className="inputButton group">
+          <EventSharp className="text-orange-400" />
+          <h4 className="opacity-80 group-hover:opacity-100">Event</h4>
+        </button>
+        <button className="inputButton group">
+          <ArticleOutlined className="text-red-400" />
+          <h4 className="opacity-80 group-hover:opacity-100">Write Article</h4>
+        </button>
       </div>
     </div>
   );
